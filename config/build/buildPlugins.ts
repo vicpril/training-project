1d7eafdd21dec4679e308c017a7d3978c58f6152
Viktor Prilepin <vicpride@gmail.com>
2023-02-09 11:51:26 +0700

    HMR added

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,8 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
 import { BuildOptions } from "./types";
 import MiniCssExtractPlugin  from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -16,6 +16,8 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: isDev
-    })
+    }),
+    new ReactRefreshWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
