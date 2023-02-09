3d52791e53863dada8fffe543f4bea82e81ee96b
Viktor Prilepin <vicpride@gmail.com>
2023-02-09 12:21:47 +0700

    babel added. babel-plugin added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -37,6 +37,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     ],
   }
 
+  const babalLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env']
+        // plugins: [
+        //   ["i18next-extract", {
+        //       "locales": [
+        //         "ru",
+        //         "en"
+        //       ],
+        //       "keyAsDefaultValue": true
+        //     }]
+        // ]
+      }
+    }
+  }
+
   const tsLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -46,6 +66,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   return [
     svgLoader,
     fileLoader,
+    babalLoader,
     tsLoader,
     cssLoader
   ]
