4db7f44024754597e81f0818f29d1a55cda5f42a
Viktor Prilepin <vicpride@gmail.com>
2023-02-11 14:46:11 +0700

    NotFoundPage added

--- src/shared/config/routerConfig/index.tsx
+++ src/shared/config/routerConfig/index.tsx
@@ -1,15 +1,18 @@
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
+import { NotFoundPage } from 'pages/NotFoundPage'
 import { RouteProps } from 'react-router-dom'
 
 export enum RouteName {
   Main = 'main',
-  About = 'about'
+  About = 'about',
+  NotFound = 'notFound'
 }
 
 export const RoutePaths: Record<RouteName, string> = {
   [RouteName.Main]: '/',
   [RouteName.About]: '/about',
+  [RouteName.NotFound]: '/*',
 }
 
 export const routerConfig: Record<RouteName, RouteProps> = {
@@ -20,5 +23,9 @@ export const routerConfig: Record<RouteName, RouteProps> = {
   [RouteName.About]: {
     element: <AboutPage />,
     path: RoutePaths.about,
+  },
+  [RouteName.NotFound]: {
+    element: <NotFoundPage />,
+    path: RoutePaths.notFound,
   },
 }
